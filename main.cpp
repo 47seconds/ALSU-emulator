310ad712f96e8942569211bf06721c74cac85aef
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-11-04 02:50:29 +0530

    changed object name to more specific

--- main.cpp
+++ main.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main() {
 
     // Testing our Logical Unit
-    LU *lu = new LU();
-    lu->setup_args(1, 0);
+    LU *logical_unit = new LU();
+    logical_unit->setup_args(1, 0);
     cout << "A: " << 1 << ", B: " << 0 << endl;
-    cout << "AND: " << static_cast<int>(lu->calc(0, 0)) << endl;
-    cout << "OR: " <<static_cast<int>(lu->calc(0, 1)) << endl;
-    cout << "XOR: " <<static_cast<int>(lu->calc(1, 0)) << endl;
-    cout << "NOT (A): " <<static_cast<int>(lu->calc(1, 1)) << endl;
-    delete lu;
+    cout << "AND: " << static_cast<int>(logical_unit->calc(0, 0)) << endl;
+    cout << "OR: " <<static_cast<int>(logical_unit->calc(0, 1)) << endl;
+    cout << "XOR: " <<static_cast<int>(logical_unit->calc(1, 0)) << endl;
+    cout << "NOT (A): " <<static_cast<int>(logical_unit->calc(1, 1)) << endl;
+    delete logical_unit;
 
     return 0;
 }
