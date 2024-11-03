8e042cd9e05ba26a9b8e1851bc949287c3ec1e4d
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-11-04 02:43:39 +0530

    testing Logical Unit

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
-#include <cmath>
+#include "header/BaseClass.h"
+#include "header/LU.h"
+using namespace std;
 
-int is2power(int n)
-{
-    // float power = log2(n);
-    if (trunc(log2(n)) == float(log2(n))) return log2(n);
-    else return -1;
-}
+// NOTE: FOR cout PURPOSES, TYPECAST TO int
 
-int main()
-{
-    int a = is2power(33);
-    std::cout << a << std::endl;
+int main() {
+
+    // Testing out Logical Unit
+    LU *lu = new LU;
+    lu->setup_args(1, 0);
+    cout << "A: " << 1 << ", B: " << 0 << endl;
+    cout << "AND: " << static_cast<int>(lu->calc(0, 0)) << endl;
+    cout << "OR: " <<static_cast<int>(lu->calc(0, 1)) << endl;
+    cout << "XOR: " <<static_cast<int>(lu->calc(1, 0)) << endl;
+    cout << "NOT (A): " <<static_cast<int>(lu->calc(1, 1)) << endl;
+    delete lu;
+
     return 0;
 }
+
+// OPTIONAL: g++ main.cpp -o main -Wall (Warning Flag: to show warnings in program / compilation)
